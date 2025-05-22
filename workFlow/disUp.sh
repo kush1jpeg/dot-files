7d831ba08551d36bde6054e4824071c1601c1001
kush1jpeg <kushuvikas@gmail.com>
2025-05-22 22:26:38 +0530

    discord Patcher

---
+++ workFlow/disUp.sh
@@ -0,0 +1,17 @@
+echo ""
+echo "Bruther stoopid discord got an update ?"
+echo ""
+echo "Lemme quickly fix that son of a bitch"
+echo ""
+echo ""
+echo "Fetching the url"
+
+curl -L "https://discord.com/api/download?platform=linux&format=deb" -o ~/Downloads/discoUpdate.deb
+
+echo "Depackaging his mom"
+
+sudo dpkg -i ~/Downloads/discoUpdate.deb
+rm -f ~/Downloads/discoUpdate.deb
+
+echo ""
+echo "we done Bruther... now lemme sleep and also fuck off.."
