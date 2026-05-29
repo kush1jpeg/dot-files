12249120c2985c81519436172e68fa9be32011aa
kush1jpeg <kushuvikas@gmail.com>
2026-05-29 12:31:30 +0530

    added tiv support keymap [Shift+Tab] + enable .env viewing inside gitRepos

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<S-Tab>", function()
+	vim.notify("opening image with tiv", vim.log.levels.INFO)
+
+	local file = vim.fn.expand("%:p")
+	vim.cmd("terminal tiv " .. vim.fn.shellescape(file))
+end, { silent = true })
--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -8,90 +8,30 @@ return {
 			vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<CR>")
 		end,
 	},
-
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				sources = {
+					explorer = {
+						hidden = true, -- Enable hidden files so .env can be seen
+						exclude = {
+							"**/.git",
+							"**/node_modules",
+							"**/.DS_Store",
+							"**/.github",
+						},
+					},
+				},
+			},
+		},
+	},
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = { "hrsh7th/cmp-emoji" },
 		opts = function(_, opts)
 			opts.sources = opts.sources or {}
 			table.insert(opts.sources, { name = "emoji" })
-		end,
-	},
-
-	{
-		"nvim-telescope/telescope.nvim",
-		keys = {
-			{
-				"<leader>fp",
-				function()
-					require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-				end,
-				desc = "Find Plugin File",
-			},
-		},
-		opts = {
-			defaults = {
-				layout_strategy = "horizontal",
-				layout_config = { prompt_position = "top" },
-				sorting_strategy = "ascending",
-				winblend = 0,
-			},
-		},
-	},
-
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				clangd = {
-					cmd = { "clangd" },
-					filetypes = { "c", "cpp", "objc", "objcpp" },
-				},
-				pyright = {},
-				emmet_ls = {},
-				tailwindcss = {},
-				denols = { autostart = false },
-				tsserver = {},
-				gopls = {
-					cmd = { "gopls" },
-					filetypes = { "go", "gomod" },
-					root_dir = require("lspconfig.util").root_pattern("go.mod", ".git"),
-					settings = {
-						gopls = {
-							analyses = {
-								unusedparams = true,
-								unusedwrite = true,
-								nilness = true,
-								shadow = true,
-							},
-							staticcheck = true,
-						},
-					},
-				},
-			},
-			setup = {
-				tsserver = function(_, opts)
-					require("typescript").setup({ server = opts })
-					return true
-				end,
-			},
-		},
-		dependencies = {
-			"jose-elias-alvarez/typescript.nvim",
-		},
-		config = function(_, opts)
-			local lspconfig = require("lspconfig")
-			for server, server_opts in pairs(opts.servers) do
-				lspconfig[server].setup(server_opts)
-			end
-
-			-- Diagnostics config goes here
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = true,
-				underline = true,
-				update_in_insert = true,
-			})
 		end,
 	},
 
